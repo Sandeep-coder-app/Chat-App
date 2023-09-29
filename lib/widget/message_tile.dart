4634b002ecc8112c7ff8429ee5407d904788b58b
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-09-29 21:26:47 +0530

    message title

--- lib/widget/message_tile.dart
+++ lib/widget/message_tile.dart
@@ -13,6 +13,21 @@ class MessageTile extends StatefulWidget {
 class _MessageTileState extends State<MessageTile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(
+        top: 4,
+        bottom: 4,
+        left: widget.sentByMe ? 0 : 24,
+        right: widget.sentByMe ? 24 : 0,
+      ),
+      alignment: widget.sentByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: widget.sentByMe
+        ? const EdgeInsets.only(left: 30)
+        : const EdgeInsets.only(right: 30),
+        padding: const EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
+        
+      ),
+    );
   }
 }

ccc9d2da725fd2c2fca025da4ceaf3834e8fe8dc
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-09-30 10:55:07 +0530

    message style

--- lib/widget/message_tile.dart
+++ lib/widget/message_tile.dart
@@ -26,7 +26,45 @@ class _MessageTileState extends State<MessageTile> {
         ? const EdgeInsets.only(left: 30)
         : const EdgeInsets.only(right: 30),
         padding: const EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
-        
+        decoration: BoxDecoration(
+          borderRadius: widget.sentByMe
+          ? const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20)
+          )
+          : const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+          color: widget.sentByMe
+          ? Colors.orange
+          : Colors.grey[700]
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.sender.toUpperCase(),
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: -0.5,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              widget.message,
+              textAlign: TextAlign.start,
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+            )
+          ],
+        ),
       ),
     );
   }

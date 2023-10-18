fff853ca2a34a4332d680b774da93f299cf954ae
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-10-18 20:32:54 +0530

    auth

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/service/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,8 +9,31 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  AuthService authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text("ChatHub",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/auth/register_page.dart
+++ lib/auth/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/service/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,8 +9,40 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+
+  bool isLoading = false;
+  final formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  String fullName = "";
+  AuthService authService = AuthService();
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "ChatHub",
+                  style: TextStyle(
+                    fontSize: 40, fontWeight: FontWeight.bold
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Image.asset("assets/chat1.png")
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

152e7417945d87262c8b05a5a9d85c1098251cdd
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-10-18 21:21:59 +0530

    auth

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/Widget/constants.dart';
 import 'package:chat_app/service/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -28,7 +29,96 @@ class _LoginPageState extends State<LoginPage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 const Text("ChatHub",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),)
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+                Image.asset("assets/chat1.png", width: 300, height: 300,),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Enter your Email",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: primaryColor,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide:const BorderSide(width: 3, color: Colors.black, style: BorderStyle.solid)
+                    )
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+
+                  //check the validation
+                  validator: (value) {
+                    return RegExp( r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                    .hasMatch(value!)
+                    ? null
+                    : "Please enter a valid email";
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Enter your Password",
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: primaryColor,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(width: 3, color: Colors.black, style: BorderStyle.solid),
+                    )
+                  ),
+
+                  validator: (value) {
+                    if(value!.length < 6) {
+                      return "Password must be at least 6 characters";
+                    } else {
+                      return null;
+                    }
+                  },
+
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: primaryColor
+                    ),
+                    child: const Text(
+                      "Sign In",
+                      textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                
               ],
             ),
           ),
